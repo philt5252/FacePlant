6a6a1863ded4c3479e3327bccf7e8d98ae0961ef
philt5252 <philt5252@gmail.com>
2014-06-24 00:51:42 -0500

    working on ground generation

---
+++ GroundFighter/Assets/Custom Scripts/SpawnScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+	public GameObject[] ObjectCollection;
+	public float MinTime;
+	public float MaxTime;
+	public bool MoveObjAfterSpawn;
+	// Use this for initialization
+	void Start () {
+		Spawn ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Spawn()
+	{
+		GameObject selectedObject = ObjectCollection [Random.Range (0, ObjectCollection.Length - 1)];
+
+		GameObject newObject = Instantiate (selectedObject, transform.position, Quaternion.identity) as GameObject;
+
+		try
+		{
+			if (MoveObjAfterSpawn) 
+			{
+				BoxCollider2D collider = newObject.GetComponent<BoxCollider2D>();
+
+				Vector3 moveVector = new Vector3(collider.size.x-.1f, 0, 0);
+				transform.Translate(moveVector);
+			}
+		}catch(UnityException ex)
+		{
+
+		}
+
+
+		Invoke ("Spawn", Random.Range (MinTime, MaxTime));
+	}
+}
