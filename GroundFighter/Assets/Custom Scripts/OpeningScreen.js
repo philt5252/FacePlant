d0b00b1d7e9417b178e39f32fa1cb40e5a6b68ee
Ammie <knapiflable@hotmail.com>
2014-06-23 20:50:10 -0500

    Opening screen stuff

---
+++ GroundFighter/Assets/Custom Scripts/OpeningScreen.js
@@ -0,0 +1,28 @@
+﻿var mainmenuSkin : GUISkin;
+var areaWidth : float;
+var areaHeight: float;
+var titleScreen : Texture;
+function OnGUI()
+{
+GUI.BeginGroup(new Rect(0,0, Screen.width, Screen.height));
+	         		GUI.DrawTexture(Rect(0, 0, Screen.width, Screen.height), titleScreen);
+	        		        		
+	        	
+		       GUI.EndGroup();
+	GUI.skin = mainmenuSkin;
+	var ScreenX = ((Screen.width *0.5) - (areaWidth *0.5));
+	var ScreenY = ((Screen.height * 0.5)-(areaHeight * 0.5));
+	
+	GUILayout.BeginArea(Rect(Screen.width-Screen.width/4, Screen.height/2, areaWidth, areaHeight));
+	
+	if(GUILayout.Button("START GAME"))
+	{
+		Application.LoadLevel("Level1");
+	}
+	
+	if(GUILayout.Button("QUIT"))
+	{
+		Application.Quit();
+	}
+	GUILayout.EndArea();
+}
