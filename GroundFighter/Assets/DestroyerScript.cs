2b2cf7db057ba367009a66c0b8670cccd9d2cdec
philt5252 <philt5252@gmail.com>
2014-06-24 11:29:45 -0500

    working on game over code

---
+++ GroundFighter/Assets/DestroyerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		Destroy (collision.gameObject);
+	}
+
+	void OnCollisionStay2D(Collision2D collision)
+	{
+		Destroy (collision.gameObject);
+	}
+}
